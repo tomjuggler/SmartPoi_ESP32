046a23233d884a4d6519eadae1a708f815250efb
tomjuggler <tomjuggler@gmail.com>
2025-11-13 18:13:33 +0200

    commented "checkit" we don't need this!

--- src/UDPHandler.cpp
+++ src/UDPHandler.cpp
@@ -29,46 +29,47 @@ void handleUDP() {
     if (len > 0) packetBuffer[len] = 0;
 
     for (int i = 0; i < NUM_PX; i++) {
-        if (checkit) {
-            Y = packetBuffer[i];
-            switch (i) {
-                case 0:
-                    if (Y != 0) checkit = false;
-                    break;
-                case 1:
-                    if (Y != 1) checkit = false;
-                    break;
-                case 2:
-                    if (Y == 3) channelChange = true;
-                    else if (Y != 2) checkit = false;
-                    break;
-                case 3:
-                    if (channelChange) {
-                        if (Y > 11 || Y < 1) {
-                            // Error handling
-                        } else {
-                            EEPROM.write(13, 1);
-                            EEPROM.commit();
-                            int newChannel2 = int(Y);
-                            EEPROM.write(13, newChannel2);
-                            EEPROM.commit();
-                            FastLED.showColor(CRGB::Magenta);
-                            channelChange = false;
-                            checkit = false;
-                        }
-                    } else {
-                        EEPROM.write(15, int(Y));
-                        EEPROM.commit();
-                        newBrightness = int(Y);
-                        FastLED.setBrightness(newBrightness);
-                        FastLED.showColor(CRGB::Blue);
-                        checkit = false;
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
+        //todo: remove this 
+        // if (checkit) {
+        //     Y = packetBuffer[i];
+        //     switch (i) {
+        //         case 0:
+        //             if (Y != 0) checkit = false;
+        //             break;
+        //         case 1:
+        //             if (Y != 1) checkit = false;
+        //             break;
+        //         case 2:
+        //             if (Y == 3) channelChange = true;
+        //             else if (Y != 2) checkit = false;
+        //             break;
+        //         case 3:
+        //             if (channelChange) {
+        //                 if (Y > 11 || Y < 1) {
+        //                     // Error handling
+        //                 } else {
+        //                     EEPROM.write(13, 1);
+        //                     EEPROM.commit();
+        //                     int newChannel2 = int(Y);
+        //                     EEPROM.write(13, newChannel2);
+        //                     EEPROM.commit();
+        //                     FastLED.showColor(CRGB::Magenta);
+        //                     channelChange = false;
+        //                     checkit = false;
+        //                 }
+        //             } else {
+        //                 EEPROM.write(15, int(Y));
+        //                 EEPROM.commit();
+        //                 newBrightness = int(Y);
+        //                 FastLED.setBrightness(newBrightness);
+        //                 FastLED.showColor(CRGB::Blue);
+        //                 checkit = false;
+        //             }
+        //             break;
+        //         default:
+        //             break;
+        //     }
+        // }
 
         X = packetBuffer[i] - 127;
         R1 = (X & 0xE0);
@@ -78,6 +79,7 @@ void handleUDP() {
         M1 = (X << 6);
         leds[i].b = M1;
     }
-
+    // Serial.print("U");
     FastLED.show();
+    yield();
 }
