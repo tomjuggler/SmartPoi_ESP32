7beafbb57f290832cc5e507bb0d0eed12e41cb1f
Your Name <you@example.com>
2026-05-05 20:57:19 +0200

    maximum newPatt should be 69 - not 62

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -183,7 +183,7 @@ void handlePatternSettings(AsyncWebServerRequest* request) {
   if(request->hasArg("patternChooserChange")) {
     int newPatt = request->arg("patternChooserChange").toInt();
     
-    if(newPatt < 0 || newPatt > 62) {
+    if(newPatt < 0 || newPatt > 69) {
         response->setCode(400);
         response->print("{\"Error\":\"Invalid pattern\"}");
         request->send(response);
